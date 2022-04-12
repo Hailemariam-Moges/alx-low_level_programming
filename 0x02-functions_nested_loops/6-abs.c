326875ec6311546b98902f2804e810261c8a5b0a
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-04-12 07:30:43 -0700

    Printing an absolute value of n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - printing the absolute value of a number
+ * @n: integer parameter n
+ * Return:0
+ */
+
+int _abs(int n)
+
+{
+	int retvalue;
+
+	if (n > 0)
+	{
+		retvalue = n
+	}
+	else
+	{
+		n = (-1) * n;
+	}
+
+	return (retvalue);
+
+}
