e86e88615fa73c818904437b648f18bffb4bd6e1
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-04-12 06:52:38 -0700

    printing 1 if it is letter, lowercase or uppercase otherwise 0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints 1 if it is letter, lowercase or uppercase.
+ * @c: integer parameter c
+ * Return:0
+ */
+
+
+int _isalpha(int c)
+{
+	int retvalue;
+
+
+
+	if ((c >= 'a' || c <= 'A') && (c <= 'z' || c <= 'Z'))
+	{
+		retvalue = 1;
+	}
+	else
+	{
+		retvalue = 0;
+	}
+
+	return (retvalue);
+
+}
