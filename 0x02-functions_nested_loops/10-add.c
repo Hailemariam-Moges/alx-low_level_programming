f29dff1b01f3ef1913b1a413eb25e3592b6fd032
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-04-12 10:04:14 -0700

    Adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n: integer parameter n
+ * @m: integer parameter m
+ * Return:0
+ */
+
+int add(int n, int m)
+
+{
+	int results;
+
+	results = n + m;
+	return (results);
+
+}
