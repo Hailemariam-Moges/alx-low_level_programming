b6a178a7f4d17afb56e079a8f51b76e7618595bd
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-04-12 06:30:13 -0700

    Print 1 if true otherwise 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _islower - print 1 if lower otherwise 0.
+ * @c: integer parameter c
+ * Return:0
+ */
+
+int _islower(int c)
+{
+	int retvalue;
+
+
+
+	if (c == 'a' && c == 'z')
+	{
+		retvalue = 1;
+
+	}
+	else
+	{
+		retvalue = 0;
+	}
+
+	return (retvalue);
+
+}
