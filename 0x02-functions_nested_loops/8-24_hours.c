4d0ea0b0f6ef8e3db2830c2157bcbc3fa2efdca8
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-04-12 08:48:23 -0700

    Printing Minutes and hours in a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,11 +19,11 @@ void jack_bauer(void)
 
 		while (min >= 0 || min < 60)
 		{
-			_putchar(hr + '0');
-			_putchar(min + '0');
+			_putchar((hr / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar(' : ');
-			_putchar(hr + '0');
-			_putchar(min + '0');
+			_putchar((hr / 10) + '0');
+			_putchar((min % 10) + '0');
 		}
 		min++;
 	}
