8814bce5b30bd5e893e24c7ca65a3c1b84b52ed5
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-04-12 15:10:00 -0700

    Fibonacci task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,16 +13,15 @@ int main(void)
 	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 98)
+	for (i < 98; i++)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 96)
+		if (i < 98)
 			printf(", ");
-		i++;
 	}
 	putchar('\n');
 	return (0);
