b91a1f848cfed85bda30474ce779e390a69c7726
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-04-20 13:19:02 -0700

    Task 11

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,21 +2,18 @@
 #include <stdio.h>
 
 /**
- *
- *  * print_buffer - prints a buffer
- *
- *   * @b: buffer.
- *
- *    * @size: size of buffer.
- *
- *     * Return: no return.
- *
- *      */
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: no return.
+ */
 
 void print_buffer(char *b, int size)
 
 {
 	int j, k, l;
+
+
 	if (size <= 0)
 		printf("\n");
 	else
