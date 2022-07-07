b5baed198219554f0adc08109545acb31d552497
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-07-07 11:07:42 -0700

    Insert Node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,42 +1,82 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *                            list at a given position.
- * @head: A pointer to the head of the dlistint_t list.
- * @index: The position to insert the new node.
- * @nw: The integer for the new node to contain.
+ * _dlistint_len - the number of elements in a linked list_t list
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * @head: list to browse
+ *
+ * Return: size of the list
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head,
-		unsigned int index, int nw)
+size_t _dlistint_len(const dlistint_t *head)
 {
-	dlistint_t *tmp = *head, *new;
+	if (head == NULL)
+		return (0);
+	else
+		return (_dlistint_len(head->next) + 1);
+}
 
-	if (index == 0)
-		return (add_dnodeint(head, nw));
+/**
+ * _get_nodeint_at_index - the nth node of a listint_t linked list
+ *
+ * @h: first element
+ * @index: element's number
+ *
+ * Return: a node
+ */
+dlistint_t *_get_nodeint_at_index(dlistint_t *h, unsigned int index)
+{
+	unsigned int iterate = 0;
 
-	for (; index != 1; index--)
+	while (h != NULL)
 	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
+		if (index == iterate)
+			return (h);
+		h = h->next;
+		iterate++;
 	}
 
-	if (tmp->next == NULL)
-		return (add_dnodeint_end(head, nw));
+	return (NULL);
+}
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ *
+ * @h: first element
+ * @idx: element's number
+ * @n: number
+ *
+ * Return: he address of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *before;
+	int length;
+
+	new = _createNode(n);
+
+	if (h == NULL || new == NULL)
 		return (NULL);
 
-	new->nw = nw;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next->prev = new;
-	tmp->next = new;
+	length = _dlistint_len(*h);
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	else if (idx > (unsigned int) length)
+	{
+		return (NULL);
+	}
+	else
+		before = _get_nodeint_at_index(*h, idx - 1);
+
+	new->prev = before;
+	new->next = before->next;
+
+	if (before->next == NULL)
+		return (add_dnodeint_end(h, n));
+	before->next->prev = new;
+	before->next = new;
 
 	return (new);
 }
