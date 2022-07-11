ab4a07124364f54a304e87b4b4b533d2667aec50
Hailemariam-Moges <hailemariammoges@gmail.com>
2022-07-11 13:10:01 -0700

    Make me Win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -q -P /tmp https://github.com/Hailemariam-Moges/alx-low_level_programming
-/main/0x18-dynamic_libraries/hack.so
+/master/0x18-dynamic_libraries/hack.so
 export LD_PRELOAD=/tmp/hack.so
